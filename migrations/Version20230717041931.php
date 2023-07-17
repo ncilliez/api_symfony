3273da590ff5b456d306e52bb21685ced9eccd9c
Nicolas Cilliez <ncilliez@gmail.com>
2023-07-17 06:27:14 +0200

    ajout de l'entitée slider

--- migrations/Version20230717041931.php
+++ migrations/Version20230717041931.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230710211606 extends AbstractMigration
+final class Version20230717041931 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,8 +23,10 @@ final class Version20230710211606 extends AbstractMigration
         $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE media_object (id INT AUTO_INCREMENT NOT NULL, file_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, categorie_id INT NOT NULL, picture_id INT NOT NULL, libelle VARCHAR(255) NOT NULL, prix NUMERIC(10, 2) NOT NULL, INDEX IDX_29A5EC27BCF5E72D (categorie_id), INDEX IDX_29A5EC27EE45BDBF (picture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE slider (id INT AUTO_INCREMENT NOT NULL, picture_id INT NOT NULL, lien VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_CFC71007EE45BDBF (picture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC27BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC27EE45BDBF FOREIGN KEY (picture_id) REFERENCES media_object (id)');
+        $this->addSql('ALTER TABLE slider ADD CONSTRAINT FK_CFC71007EE45BDBF FOREIGN KEY (picture_id) REFERENCES media_object (id)');
     }
 
     public function down(Schema $schema): void
@@ -32,8 +34,10 @@ final class Version20230710211606 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC27BCF5E72D');
         $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC27EE45BDBF');
+        $this->addSql('ALTER TABLE slider DROP FOREIGN KEY FK_CFC71007EE45BDBF');
         $this->addSql('DROP TABLE categorie');
         $this->addSql('DROP TABLE media_object');
         $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE slider');
     }
 }
---
+++ src/Entity/Slider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\SliderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SliderRepository::class)]
+#[ApiResource]
+class Slider
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?MediaObject $picture = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lien = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPicture(): ?MediaObject
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?MediaObject $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): self
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
